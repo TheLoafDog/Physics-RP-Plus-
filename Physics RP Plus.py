73af2cea38e3d49d39cc6e5e93c3989a4a6847f3
fahadw4 <fahad.wasim06@gmail.com>
2024-09-26 17:15:48 +0100

    Update Physics RP Plus.py

--- Physics RP Plus.py
+++ Physics RP Plus.py
@@ -2,6 +2,8 @@ from tkinter import *
 
 darkGrey = '#ababab'
 
+requiredPracticalList = ["RP1 Stationary Waves on a String", "RP2 Interference Effects", "RP3 Determination of g", "RP4 Determination of Young's Modulus", "RP5 Determination of Resistivity of a Wire", "RP6 Internal resistance and emf", "RP7 Simple Harmonic Motion", "RP8 Boyle's Law", "RP9 Charging and Discharging Capacitors", "RP10 Magnetic Force on a Wire", "RP11 Magnetic Flux Linkage", "RP12 Inverse Square Law"]
+
 def main():
     root = Tk()
     
@@ -22,6 +24,10 @@ def main():
     sideBarButtonFrame = Frame(master = sideBar, width = 150, height = 625, bd = 2, relief = "groove", bg = darkGrey)
     sideBarButtonFrame.place(relx = 0.5, rely = 0.525, anchor = CENTER)
     
+    for i in range(0,12):
+        newSideBarButton = Button(master = sideBarButtonFrame, width = 20, height = 2, bd = 2, relief = "raised", text = requiredPracticalList[i], wraplength = 125)
+        newSideBarButton.grid(row = i, column = 0, padx = 5, pady = 5)
+    
     mainFrame = Frame(master = root, width = 880, height = 720, bd = 2, relief = "groove")
     mainFrame.grid(row = 0, column = 1, sticky = "nsew")
     
@@ -30,3 +36,4 @@ def main():
     
     
 main()
+
