1d954efb1d44fd8e8f4f86cd5ec6ac0908bf0b9e
fahadw4 <fahad.wasim06@gmail.com>
2024-09-26 15:04:03 +0100

    Update Physics RP Plus.py

--- Physics RP Plus.py
+++ Physics RP Plus.py
@@ -16,6 +16,12 @@ def main():
     sideBar = Frame(master = root, width = 200, height = 720, bd = 2, relief = "groove", bg = darkGrey)
     sideBar.grid(row = 0, column = 0, sticky = "nsew")
     
+    sideBarTitle = Label(master = sideBar, width = 20, height = 3, text = "Physics RP Plus", bg = darkGrey, font = ('Arial', 12))
+    sideBarTitle.place(relx = 0.5, rely = 0.05, anchor = CENTER)
+    
+    sideBarButtonFrame = Frame(master = sideBar, width = 150, height = 625, bd = 2, relief = "groove", bg = darkGrey)
+    sideBarButtonFrame.place(relx = 0.5, rely = 0.525, anchor = CENTER)
+    
     mainFrame = Frame(master = root, width = 880, height = 720, bd = 2, relief = "groove")
     mainFrame.grid(row = 0, column = 1, sticky = "nsew")
     
